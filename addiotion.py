ebfe9953140bf6e501d851c56a84621d4fc1cba4
karinkirajesh <karinkirajesh55@gmail.com>
2025-08-05 14:23:37 +0530

    new commit

--- addiotion.py
+++ addiotion.py
@@ -2,6 +2,6 @@ num1=int(input("enter the first number :"))
 num2=int(input("enter the second number :"))
 def add(num1,num2):
     sum=num1+num2
-    print("sum of given numbers is ",sum)
+    print("sum of the given numbers is ",sum)
 
 add(num1,num2)
